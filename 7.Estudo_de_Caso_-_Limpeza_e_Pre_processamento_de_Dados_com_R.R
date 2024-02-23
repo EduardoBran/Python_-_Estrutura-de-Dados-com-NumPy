1b4bd45f3346797253a642e11f41d38a7fc17cdf
EduardoBran <eduardo.ads1814@gmail.com>
2024-02-23 20:19:20 -0300

    continuacao da limpeza e pre-processamento - variaveis categoricas

--- 7.Estudo_de_Caso_-_Limpeza_e_Pre_processamento_de_Dados_com_R.R
+++ 7.Estudo_de_Caso_-_Limpeza_e_Pre_processamento_de_Dados_com_R.R
@@ -22,32 +22,24 @@ dim(dados)
 
 
 ## Verificando Dados Ausentes
-colSums(is.na(dados))
+#colSums(is.na(dados))
 head(dados)
 head(dados$issue_d)
 head(dados$int_rate)
 
 
-
 #  -> Diferentemente do Python e NumPy , o R carrega todos os dados como chr e assim não temos valores considerados ausentes e sim como chr ""
-
-
-
-#### Repetindo o Processo do Estudo de Caso para diferenciar colunas do tipo string das do tipo numérica
 
 
 ## Verificando valores ausentes (aqui serão considerados dados como chr "" e caracteres especiais)
 
-# Colunas com valores vazios (chr "")
-sapply(dados, function(x) sum(x == ""))
-
-# Colunas com caracteres especiais
-sapply(dados, function(x) sum(grepl("[^\\x20-\\x7E]", x)))
+sapply(dados, function(x) sum(x == ""))                     # Colunas com valores vazios (chr "")
+sapply(dados, function(x) sum(grepl("[^\\x20-\\x7E]", x)))  # Colunas com caracteres especiais
 
 
-## Início Tratamento Valores Ausentes
+## Início Tratamento Valores Ausentes e Caracter Especial (transformando valores ausentes e caracter especial em NA)
 
-# Função ajustada para limpar colunas com critérios específicos (iremos adicionar valores NA para chr"" e coluna int_rate)
+# Função ajustada para limpar colunas com critérios específicos (iremos adicionar valores NA para chr"" e colunas int_rate e installment)
 limpar_colunas <- function(coluna, nome_coluna) {
   coluna[coluna == ""] <- NA                                         # Substituir strings vazias por NA para todas as colunas
   if (nome_coluna == "int_rate" || nome_coluna == "installment") {   # Para a coluna int_rate, substituir strings com caracteres especiais por NA
@@ -59,9 +51,10 @@ limpar_colunas <- function(coluna, nome_coluna) {
 dados_limpos <- as.data.frame(mapply(limpar_colunas, dados, names(dados), SIMPLIFY = FALSE))
 head(dados_limpos)
 str(dados_limpos)
+rm(limpar_colunas)
 
 
-## Convertendo Colunas Para Tipo String e Numérico
+## Transformando Colunas Para Numérico (e assim diferenciar dasquelas que serão do tipo factor)
 
 colunas_para_converter <- c("id", "loan_amnt", "funded_amnt", "int_rate", "installment", "total_pymnt") # Lista das colunas para converter para numérico
 
@@ -69,8 +62,231 @@ colunas_para_converter <- c("id", "loan_amnt", "funded_amnt", "int_rate", "insta
 for (coluna in colunas_para_converter) {
   dados_limpos[[coluna]] <- as.numeric(dados_limpos[[coluna]])
 }
-
 str(dados_limpos)
+rm(colunas_para_converter, coluna)
+
+
+
+#### Tratando Colunas do tipo String ('issue_d' 'loan_status' 'term' 'grade' 'sub_grade' 'verification_status' 'url' 'addr_state')
+
+
+### Pré-Processamento da Variável issue_date com Label Encoding (não foi tratado valor ausente)
+
+# Modificando nome
+# which(names(dados_limpos) == "issue_d")
+names(dados_limpos)[2] <- "issue_date"
+
+# Verificando valores únicos
+unique(dados_limpos$issue_date)
+
+# Remover "-15" de issue_date
+dados_limpos$issue_date <- gsub("-15", "", dados_limpos$issue_date)
+
+# Definindo o vetor associativo para mapear os nomes dos meses para números
+meses_para_numeros <- c(Jan = 1, Feb = 2, Mar = 3, Apr = 4, May = 5, Jun = 6, Jul = 7, Aug = 8, Sep = 9, Oct = 10, Nov = 11, Dec = 12)
+
+# Loop usando sapply para substituir os nomes dos meses pelos números correspondentes
+dados_limpos$issue_date <- sapply(dados_limpos$issue_date, function(x) {
+  if (!is.na(x) && x %in% names(meses_para_numeros)) {
+    meses_para_numeros[x]
+  } else {
+    NA  # Atribuir NA se não for um mês válido ou se for NA originalmente
+  }
+})
+
+# Converter a coluna issue_date para factor
+dados_limpos$issue_date <- as.factor(dados_limpos$issue_date)
+
+# Verificar as mudanças
+table(dados_limpos$issue_date, useNA = "ifany")
+rm(meses_para_numeros)
+
+
+## Pré-Processamento da Variável loan_status com Binarização (valor ausente tratado - todo valor ausente considerado como 0)
+
+# Verificando valores únicos
+unique(dados_limpos$loan_status)
+
+# Modificar loan_status para 0 e 1 conforme as condições
+dados_limpos$loan_status <-
+  ifelse(dados_limpos$loan_status %in% c(NA, "Charged Off", "Default", "Late (16-30 days)"), 
+      0, 
+  ifelse(is.na(dados_limpos$loan_status), 
+      NA, 
+      1)
+)
+
+
+# Converter a coluna loan_status para factor
+dados_limpos$loan_status <- as.factor(dados_limpos$loan_status)
+
+# Verificar as mudanças
+table(dados_limpos$loan_status, useNA = "ifany")
+
+
+
+### Pré-Processamento da Variável term com Limpeza de String (valores ausentes tratados - substituindo valores NA pelo maior valor)
+
+# Modificando nome
+names(dados_limpos)[6] <- "term_months"
+
+# Verificando valores únicos
+unique(dados_limpos$term_months)
+
+# Remover " months" de e "" no início de term_months
+dados_limpos$term_months <- trimws(gsub(" months", "", dados_limpos$term_months))
+
+# Substituir NA pelo maior valor ("60") em term_months
+dados_limpos$term_months[is.na(dados_limpos$term_months)] <- "60"
+
+# Converter a coluna term_months para factor
+dados_limpos$term_months <- as.factor(dados_limpos$term_months)
+
+# Verificar as mudanças
+table(dados_limpos$term_months, useNA = "ifany")
+
+
+
+### Pré-Processamento das Variáveis grade e sub_grade com Dicionário (variaveis ausentes tratadas)
+
+# Verificando valores únicos
+unique(dados_limpos$grade)
+unique(dados_limpos$sub_grade)
+
+# - Analisando a variável foi constatado que elas possuem uma relação, com sub_grade como uma espécia de subcategoria de grade
+#   Não podemos e não vamos manter duas variáveis com mesmas informações. Vamos manter apenas sub_grade pois temos mais informações.
+
+
+# Loop para ajustar a variável sub_grade
+# -> O loop identifica casos em que a variável sub_grade está vazia ('') e a variável grade correspondente tem um valor.
+#    Nesses casos, atribui-se à sub_grade o valor da grade com um '5' adicionado ao final.
+
+unique_grades <- na.omit(unique(dados_limpos$grade)) # gravando objeto com valores únicos da variável grade sem NA
+
+for(grade in unique_grades) {
+  i <- which(is.na(dados_limpos$sub_grade) & dados_limpos$grade == grade) # Índices onde sub_grade está vazia e grade corresponde ao valor atual do loop
+  
+  dados_limpos$sub_grade[i] <- paste0(grade, "5") # Atribuir a sub_grade o valor da grade com um '5' adicionado ao final para esses índices
+}
+rm(grade, unique_grades, i)
+
+# Verificar se a substituição foi realizada corretamente
+table(dados_limpos$sub_grade, useNA = "ifany")
+
+
+# Substituir valores NA por 'H1' em sub_grade
+dados_limpos$sub_grade[is.na(dados_limpos$sub_grade)] <- 'H1'
+
+# Removendo Variável Grade
+dados_limpos$grade <- NULL
+
+# Verificar se a substituição foi realizada corretamente
+table(dados_limpos$sub_grade, useNA = "ifany")
+str(dados_limpos)
+
+# Garantir que 'sub_grade' seja um fator e os níveis estejam na ordem correta (em python precisou criar um dicionário)
+dados_limpos$sub_grade <- factor(dados_limpos$sub_grade, levels = c("A1", "A2", "A3", "A4", "A5", 
+                                                                    "B1", "B2", "B3", "B4", "B5", 
+                                                                    "C1", "C2", "C3", "C4", "C5", 
+                                                                    "D1", "D2", "D3", "D4", "D5", 
+                                                                    "E1", "E2", "E3", "E4", "E5", 
+                                                                    "F1", "F2", "F3", "F4", "F5", 
+                                                                    "G1", "G2", "G3", "G4", "G5", "H1"))
+
+# Verificando Tipo dos Dados
+str(dados_limpos)
+summary(dados_limpos$sub_grade)
+
+
+
+## Pré-Processamento da Variável verification_status com Binarização (valor ausente tratado - todo valor ausente considerado como 0)
+
+# Verificando valores únicos antes da modificação
+unique(dados_limpos$verification_status)
+
+# Binarizar a variável verification_status corretamente
+dados_limpos$verification_status <- 
+  ifelse(dados_limpos$verification_status == "Not Verified" | is.na(dados_limpos$verification_status), 0, 1)
+
+# Converter a coluna loan_status para factor
+dados_limpos$verification_status <- as.factor(dados_limpos$verification_status)
+
+# Verificar as mudanças aplicadas
+table(dados_limpos$verification_status, useNA = "ifany")
+
+
+
+### Pré-Processamento da Variável url com Extração de ID
+
+# - Constado que o valor final da informação da variável URL é possivelmente o mesmo valor da variável ID (faremos a verificação)
+
+head(dados_limpos$url, 3)
+head(dados_limpos$id, 3)
+
+# Extrair o ID da URL
+dados_limpos$url <- gsub("https://www.lendingclub.com/browse/loanDetail.action\\?loan_id=", "", dados_limpos$url)
+
+# Converter a coluna url para numérico para facilitar a comparação
+dados_limpos$url <- as.numeric(dados_limpos$url)
+head(dados_limpos$url, 3)
+
+# Comparar os IDs extraídos da URL com a coluna id
+identical(dados_limpos$url, dados_limpos$id)
+
+# Removendo a Variável url pois tem as mesmas informações da variável id
+dados_limpos$url <- NULL
+
+# Tipo de Dados
+str(dados_limpos)
+
+
+
+### Pré-Processamento da Variável address com Categorização (dados ausentes ficaram com valor 0)
+
+# Ajustar nome da coluna
+# which(names(dados_limpos) == "addr_state")
+names(dados_limpos)[11] <- "state_address"
+
+# Substituir valores ausentes por "NA"
+dados_limpos$state_address[is.na(dados_limpos$state_address)] <- "NA"
+
+# Contar e visualizar a ordenação dos estados
+sort(table(dados_limpos$state_address), decreasing = TRUE)
+
+
+# Definir vetores por região
+states_west <- c('WA', 'OR', 'CA', 'NV', 'ID', 'MT', 'WY', 'UT', 'CO', 'AZ', 'NM', 'HI', 'AK')
+states_south <- c('TX', 'OK', 'AR', 'LA', 'MS', 'AL', 'TN', 'KY', 'FL', 'GA', 'SC', 'NC', 'VA', 'WV', 'MD', 'DE', 'DC')
+states_midwest <- c('ND', 'SD', 'NE', 'KS', 'MN', 'IA', 'MO', 'WI', 'IL', 'IN', 'MI', 'OH')
+states_east <- c('PA', 'NY', 'NJ', 'CT', 'MA', 'VT', 'NH', 'ME', 'RI')
+
+# Categorizar estados por região
+dados_limpos$state_address <- ifelse(dados_limpos$state_address %in% states_west, 1,
+                                    ifelse(dados_limpos$state_address %in% states_south, 2,
+                                           ifelse(dados_limpos$state_address %in% states_midwest, 3,
+                                                  ifelse(dados_limpos$state_address %in% states_east, 4, 0))))
+rm(states_west, states_south, states_midwest, states_east)
+
+# Converter a coluna state_address para factor
+dados_limpos$state_address <- as.factor(dados_limpos$state_address)
+
+# Verificar as mudanças
+table(dados_limpos$state_address, useNA = "ifany")
+
+# Tipo de dados
+str(dados_limpos)
+summary(dados_limpos)
+
+## Verificando Dados Ausentes
+colSums(is.na(dados_limpos))
+
+# variáveis com valores ausentes -> issue_date, loan_amnt, funded_amnt, int_rate, installment e total_pymnt
+
+
+
+
+#### Tratando Colunas do tipo Numérica
+
 
 
 
